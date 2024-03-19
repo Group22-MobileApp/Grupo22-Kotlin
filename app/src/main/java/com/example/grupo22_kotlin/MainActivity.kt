f666024cf76d047b7ccd964e91acd249ec861ed2
montejs3 <98852422+montejs3@users.noreply.github.com>
2024-03-19 11:05:33 -0500

    packagin Structure, con peqeuña app para testear que todo funcionab, toca borrar esp

--- app/src/main/java/com/example/grupo22_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/grupo22_kotlin/MainActivity.kt
@@ -1,46 +1,91 @@
 package com.example.grupo22_kotlin
 
+import androidx.compose.ui.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.grupo22_kotlin.ui.theme.Grupo22KotlinTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Grupo22KotlinTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                var name by remember {
+                    mutableStateOf("")
                 }
+
+                var names by remember {
+                    mutableStateOf(listOf<String>())
+                }
+
+                Column (modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+                ){
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                    ){
+                        OutlinedTextField(
+                            value = name,
+                            onValueChange ={ text ->
+                                name = text
+                            } )
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Button(onClick = {
+                            if(name.isNotBlank()){
+                                names = names + name
+                                name = ""
+                            }
+
+                        }) {
+                            Text(text = "Add")
+                        }
+                    }
+
+                    LazyColumn {
+                       items(names){currentName ->
+                           Text(text = currentName,
+                               modifier = Modifier
+                                   .fillMaxWidth()
+                                   .padding(16.dp))
+                           Divider()
+                       }
+                    }
+
+                }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Lisan Algaid testing $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Grupo22KotlinTheme {
-        Greeting("Android")
-    }
-}
+
