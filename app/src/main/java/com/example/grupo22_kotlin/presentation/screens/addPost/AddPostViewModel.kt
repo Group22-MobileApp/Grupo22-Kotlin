4ff8a15767cbb7dfcaca4b02b906c7120694ed61
Camilo Colmenares <j.colmenares@uniandes.edu.co>
2024-03-22 00:26:56 -0500

    View Model for the AddPost correctly implemented

--- app/src/main/java/com/example/grupo22_kotlin/presentation/screens/addPost/AddPostViewModel.kt
+++ app/src/main/java/com/example/grupo22_kotlin/presentation/screens/addPost/AddPostViewModel.kt
@@ -1,9 +1,90 @@
 package com.example.grupo22_kotlin.presentation.screens.addPost
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class AddPostViewModel @Inject constructor(): ViewModel() {
+
+    var name: MutableState<String> = mutableStateOf("")
+    var isNameValid: MutableState<Boolean> = mutableStateOf(false)
+    var nameErrMsg: MutableState<String> = mutableStateOf("")
+
+    var price: MutableState<String> = mutableStateOf("")
+    var isPriceValid: MutableState<Boolean> = mutableStateOf(false)
+    var priceErrMsg: MutableState<String> = mutableStateOf("")
+
+    var description: MutableState<String> = mutableStateOf("")
+    var isDescriptionValid: MutableState<Boolean> = mutableStateOf(false)
+    var descriptionErrMsg: MutableState<String> = mutableStateOf("")
+
+    var category: MutableState<String> = mutableStateOf("")
+    var isCategoryValid: MutableState<Boolean> = mutableStateOf(false)
+    var categoryErrMsg: MutableState<String> = mutableStateOf("")
+
+    var isEnabledPostButton = false
+
+    var isEnabledCheckYes = false
+    var isEnabledCheckNo = false
+    var isEnabledCheckNew = false
+    var isEnabledCheckUsed = false
+
+    fun enabledLoginButton() {
+        isEnabledPostButton = isNameValid.value &&
+                isPriceValid.value &&
+                isDescriptionValid.value &&
+                isCategoryValid.value &&
+                (isEnabledCheckNew || isEnabledCheckUsed) &&
+                (isEnabledCheckYes || isEnabledCheckNo)
+    }
+
+    fun validateName() {
+        if (name.value.length >= 5) {
+            isNameValid.value = true
+            nameErrMsg.value = ""
+        } else {
+            isNameValid.value = false
+            nameErrMsg.value = "A name needs at least 5 characters"
+        }
+
+        enabledLoginButton()
+    }
+
+    fun validatePrice() {
+        if (price.value.toDouble() >= 1000) {
+            isPriceValid.value = true
+            priceErrMsg.value = ""
+        } else {
+            isPriceValid.value = false
+            priceErrMsg.value = "The minimum transaction should be 1000 pesos"
+        }
+
+        enabledLoginButton()
+    }
+
+    fun validateDescription() {
+        if (description.value.length >= 6) {
+            isDescriptionValid.value = true
+            descriptionErrMsg.value = ""
+        } else {
+            isDescriptionValid.value = false
+            descriptionErrMsg.value = "A description needs at least 6 characters"
+        }
+
+        enabledLoginButton()
+    }
+    fun validateCategory() {
+        if (category.value.length >= 1) {
+            isCategoryValid.value = true
+            categoryErrMsg.value = ""
+        } else {
+            isCategoryValid.value = false
+            category.value = "That career must not be empty"
+        }
+
+        enabledLoginButton()
+    }
 }
