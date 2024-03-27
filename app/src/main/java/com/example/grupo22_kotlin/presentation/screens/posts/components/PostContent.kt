51c3124064ade3f25fbc8468a6496530961b082f
Camilo Colmenares <j.colmenares@uniandes.edu.co>
2024-03-27 14:36:54 -0500

    listing posts is working - now we need to work on the post details presentation

---
+++ app/src/main/java/com/example/grupo22_kotlin/presentation/screens/posts/components/PostContent.kt
@@ -0,0 +1,26 @@
+package com.example.grupo22_kotlin.presentation.screens.posts.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.grupo22_kotlin.domain.model.Post
+
+@Composable
+fun PostContent(posts: List<Post>){
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 20.dp, end = 20.dp, top = 20.dp, bottom = 55.dp)
+    ) {
+        items(
+            items = posts
+        ) { post ->
+            Text(text = post.name)
+        }
+    }
+}
