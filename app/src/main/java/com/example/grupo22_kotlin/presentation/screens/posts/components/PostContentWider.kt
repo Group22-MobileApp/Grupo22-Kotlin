9884b60bd5778e8d7ccbbc69c3d73b963ca06007
dcgonzalezp <88750590+dcgonzalezp@users.noreply.github.com>
2024-04-25 17:42:46 -0500

    View finished

--- app/src/main/java/com/example/grupo22_kotlin/presentation/screens/posts/components/PostContentWider.kt
+++ app/src/main/java/com/example/grupo22_kotlin/presentation/screens/posts/components/PostContentWider.kt
@@ -1,31 +1,30 @@
 package com.example.grupo22_kotlin.presentation.screens.posts.components
 
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.example.grupo22_kotlin.domain.model.Post
 
 @Composable
-fun PostContentForYou(posts: List<Post>, navController: NavHostController){
+fun PostContentWider(posts: List<Post>, navController: NavHostController){
     LazyColumn(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(start = 20.dp, end = 20.dp, top = 20.dp, bottom = 20.dp)
-            .height(500.dp)
+            .padding(bottom = 20.dp)
+            .heightIn(max = 2000.dp)
     ) {
         items(
-            items = posts
-        ) { post ->
-            PostCard(post = post, navController)
+            count = posts.size
+        ) { i ->
+            PostCardWider(post = posts[i], navController)
         }
     }
 }
--- app/src/main/java/com/example/grupo22_kotlin/presentation/screens/postsFetch/components/GetPostsByCategory.kt
+++ app/src/main/java/com/example/grupo22_kotlin/presentation/screens/postsFetch/components/GetPostsByCategory.kt
@@ -2,7 +2,10 @@ package com.example.grupo22_kotlin.presentation.screens.postsFetch.components
 
 import android.widget.Toast
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
@@ -13,31 +16,37 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.grupo22_kotlin.domain.model.Response
+import com.example.grupo22_kotlin.presentation.components.TitleText
 import com.example.grupo22_kotlin.presentation.screens.posts.PostViewModel
 import com.example.grupo22_kotlin.presentation.screens.posts.components.PostContent
+import com.example.grupo22_kotlin.presentation.screens.posts.components.PostContentWider
 import com.example.grupo22_kotlin.presentation.screens.postsFetch.PostsFetchViewModel
 
 @Composable
-fun GetPostsByCategory(navController: NavHostController, viewModel: PostsFetchViewModel = hiltViewModel()) {
-
-    when(val response = viewModel.postsCategoryResponse) {
+fun GetPostsByCategory(
+    navController: NavHostController,
+    viewModel: PostsFetchViewModel = hiltViewModel()
+) {
+    when (val response = viewModel.postsCategoryResponse) {
         // MOSTRAR QUE SE ESTA REALIZANDO LA PETICION Y TODAVIA ESTA EN PROCESO
         Response.Loading -> {
             Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
                 CircularProgressIndicator(modifier = Modifier.size(80.dp))
             }
         }
+
         is Response.Success -> {
-            PostContent(posts = response.data, navController)
+            PostContentWider(posts = response.data, navController)
         }
 
         is Response.Failure -> {
             Toast.makeText(
                 LocalContext.current,
                 response.exception?.message ?: "Somethings wrong",
-                Toast.LENGTH_LONG).show()
+                Toast.LENGTH_LONG
+            ).show()
         }
+
         else -> {}
     }
-
 }
